9eda9f83fb038fcbf47bc1f53956aa8c7d235d2e
vu <truonganhvu205@gmail.com>
2024-01-30 16:30:07 +0700

    commit

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 pip install -r requirements.txt
 
-python3 manage.py makemigrations
-python3 manage.py migrate
+python3.10 manage.py makemigrations
+python3.10 manage.py migrate
 
-python3 manage.py collectstatic
+python3.10 manage.py collectstatic
